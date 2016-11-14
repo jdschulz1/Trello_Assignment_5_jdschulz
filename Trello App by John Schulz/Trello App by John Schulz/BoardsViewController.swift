3664a49fd6efd639c1e2265caa5462c5e585ab75
John Schulz <jdschulz1@gmail.com>
2016-11-13 19:09:52 -0600

    Added classes for Boards, Lists and Cards as UI elements and started following book examples for retrieving JSON data from URL

--- Trello App by John Schulz/Trello App by John Schulz/BoardsViewController.swift
+++ Trello App by John Schulz/Trello App by John Schulz/BoardsViewController.swift
@@ -12,4 +12,36 @@ class BoardsViewController: UIViewController {
     
     @IBOutlet var boardView: UICollectionView!
     
+    let session: NSURLSession = {
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        return NSURLSession(configuration: config)
+    }()
+    
+    func getBoards() {
+        let url = TrelloAPI.allBoardsURL()
+        let request = NSURLRequest(URL: url)
+        let task = session.dataTaskWithRequest(request) {
+            (data, response, error) -> Void in
+            
+            if let jsonData = data {
+                if let jsonString = NSString(data: jsonData,
+                                             encoding: NSUTF8StringEncoding) {
+                    print(jsonString)
+                }
+            }
+            else if let requestError = error {
+                print("Error fetching Trello Boards: \(requestError)")
+            }
+            else {
+                print("Unexpected error with the request")
+            }
+        }
+        task.resume()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        getBoards()
+    }
 }
---
+++ Trello App by John Schulz/Trello App by John Schulz/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  Trello App by John Schulz
+//
+//  Created by admin on 11/13/16.
+//  Copyright © 2016 edu.oc.eagles. All rights reserved.
+//
+
+import UIKit
+
+class Card: UICollectionViewCell {
+    
+}
